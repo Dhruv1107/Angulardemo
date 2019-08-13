a165e681775ef5277d70e185f9b29e2985409b6f
dhruv <dhruvsr4@gmail.com>
2019-08-14 00:31:50 +0530

    section 2 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ServerComponent } from "./server/server.component";
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   //add components
-  declarations: [AppComponent, ServerComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
   //add some other modules
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
